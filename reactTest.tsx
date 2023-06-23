405c8791913342032d1ed71be9a279f7a3168657
euni.ryu <euni.ryu97@gmail.com>
2023-06-23 11:50:26 +0900

    react형태의 코드 nest.js에서 띄우기

---
+++ reactTest.tsx
@@ -0,0 +1,10 @@
+
+
+const content =
+    `<div style={display: 'flex', justifyContent: 'center'}>
+      <h1>Project B</h1>
+      <div>Hello World~!~!~!</div>
+    </div>`
+
+
+export default content;
