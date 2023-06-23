e61d8992e87a16cf17446e3bd30a5d9e1c119564
euni.ryu <euni.ryu97@gmail.com>
2023-06-23 15:15:35 +0900

    nest test중

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.Page();
+  @Get('home')
+  public async showHome(@Req() req: Request, @Res() res: Response) {
+    await this.appService.handler(req, res);
   }
+  
 }
